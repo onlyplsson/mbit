e41faf7f3544bfcb0ee12e88ac21c04b56353222
onlyplsson <onlyplsson@gmail.com>
2021-12-05 18:16:40 +0900

    1205

---
+++ app/src/main/java/com/example/mbit/Chat.java
@@ -0,0 +1,22 @@
+package com.example.mbit;
+
+public class Chat {
+    private String sChatNick;
+    private String sChatConetent;
+
+    public String getsChatNick() {
+        return sChatNick;
+    }
+
+    public void setsChatNick(String sChatNick) {
+        this.sChatNick = sChatNick;
+    }
+
+    public String getsChatConetent() {
+        return sChatConetent;
+    }
+
+    public void setsChatConetent(String sChatConetent) {
+        this.sChatConetent = sChatConetent;
+    }
+}
---
+++ app/src/main/java/com/example/mbit/ChatAdapter.java
@@ -0,0 +1,61 @@
+package com.example.mbit;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.ViewHolder> {
+    private ArrayList<Chat> mData = null;
+
+    public ChatAdapter(ArrayList<Chat> data) {
+        mData = data;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView nick;
+        TextView content;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+
+            nick = itemView.findViewById(R.id.tv_ChatNick);
+            content = itemView.findViewById(R.id.tv_ChatContent);
+        }
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View view = inflater.inflate(R.layout.recyclerview_chat, parent, false);
+        ChatAdapter.ViewHolder vh = new ChatAdapter.ViewHolder(view);
+
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Chat chat = mData.get(position);
+
+        holder.nick.setText(chat.getsChatNick());
+        holder.content.setText(chat.getsChatConetent());
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+
+
+
+}
